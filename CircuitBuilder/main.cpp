c09130912a3476d1d3cda53f9b0cc4a0d9bb36fe
Master47474 <meoro@hotmail.com.au>
2019-04-18 09:39:22 +1000

    Added Cpp files
    Added main .cpp file

---
+++ CircuitBuilder/main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+#define GAY 333.2
+
+int main(){
+  cout << "Hello world" << endl;
+  cout << GAY << endl;
+  unsigned int x = 1;
+  int b;
+  int a = (b=3,b+2);
+  cout << a << endl;
+  {
+    cout << ((1<2) ? "nigger" : "White man") << endl;
+    cout << x << endl;
+  }
+  cout << x << endl;
+  return 0;
+}
