ed05a327cc7885a338e8db01f2cf8d88a5140d1c
Marcus Facchino <meoro@hotmail.com.au>
2019-05-02 12:16:37 +1000

    Pi.cpp

---
+++ FITRecreaating/Assesment1/pi.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+#define PI PI
+
+using namespace std;
+
+struct FuncInfo{
+  int aprox;
+  int steps;
+};
+
+
+int main(void){
+
+  return 0;
+}
+
+
+FuncInfo Basel(int& prec){
+  double aprox = 0.0f;
+  for(int i = 1; aprox - PI >= prec ;i++){
+    
+  }
+}
