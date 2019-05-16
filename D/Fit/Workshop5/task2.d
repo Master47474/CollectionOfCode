a46bcf2fe8df5bfd23e0b53fabdf4a156ee9362e
Marcus Facchino <meoro@hotmail.com.au>
2019-05-16 17:56:37 +1000

    Update task2.d

--- D/Fit/Workshop5/task2.d
+++ D/Fit/Workshop5/task2.d
@@ -1,12 +1,15 @@
 import std.stdio;
 import std.conv;
 import std.file;
+import std.array;
 
 
 void main(){
-  byte[] buffer;
-  buffer.length = 1024;
-  auto file = File("Tiny.txt", "r");
-  auto data = file.rawRead(buffer);
-  writeln(data);
+  auto file = File("Tiny.txt");
+  auto range = file.byLine();
+  foreach (line; range){
+    line = line[0 .. $-1];
+    string[] lin = split(to!string(line),",");
+    writeln(to!int(lin[0])," Kms, $", to!float(lin[1]));
+  }
 }
