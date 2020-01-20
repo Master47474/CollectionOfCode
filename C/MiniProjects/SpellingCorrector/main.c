1a8e6a3b68a635bda5b87c494b889f849c89e803
Master47474 <meoro@hotmail.com.au>
2020-01-20 18:32:23 +1100

    code is complete but bugs
    all words are being flagged as misspelled

--- C/MiniProjects/SpellingCorrector/main.c
+++ C/MiniProjects/SpellingCorrector/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <windows.h>
+#include<string.h>
+
 
 // file maxLINES
 #define MAXLINES 194433 // max length of english file
@@ -9,9 +10,6 @@
 
 
 
-void loading();
-void loadDictionary();
-
 struct lineno{
   char letter;
   int fileline;
@@ -19,38 +17,39 @@ struct lineno{
 
 // linked list for misspelled words
 struct Node{
-  char *data;
+  char data[45];
   struct Node* next;
 };
 
 
+
+void printLL(struct Node* n);
+void loading();
+void loadDictionary();
+int isSpelledCorrectly(char word[]);
+void insertNode(struct Node* head, char val[]);
+
+
+char dictionary[MAXLINES][45];
+
 int main(){
-
   loadDictionary();
-
   // let person write from command line, (ctrl z is exit from EOF)
-  struct Node* head = NULL;
-  struct Node* second = NULL;
-  struct Node* tail = NULL;
 
+  struct Node* head = NULL;
   head = (struct Node*)malloc(sizeof(struct Node));
-  second = (struct Node*)malloc(sizeof(struct Node));
-  third = (struct Node*)malloc(sizeof(struct Node));
 
   // assign head and tail to be place holders
-  head->data = "\0";
-  head->next = tail;
-
-  tail->data = '\0';
-  tail->next = NULL;
-
+  strcpy(head->data, "M");
+  head->next = NULL;
 
   int c, nl, nw, state;
-  char currentword[] = "";
+  char currentword[45];
   int i = 0;
 
   state = OUTPP;
   nl = 0;
+  printf("WE ABOUT TO LOOP NIGGA\n");
   while((c = getchar()) != EOF){
     if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') )
       currentword[i++] = c;
@@ -58,19 +57,35 @@ int main(){
       ++nl;
     if(c == ' ' || c == '\n' || c == '\t'){
       if(state == INPP && i != 0)
-        currentword[i] = '0';
+        currentword[i] = '\0';
       i = 0;
       state = OUTPP;
       // test if word is spelled correctly i.e find it in the file
+      if (isSpelledCorrectly(currentword) == 0){
+        insertNode(head, currentword);
+        printf("INSERTED WORD WRONG HERE \"%s\"\n", currentword);
+      }
     }else if(state == OUTPP){
       state = INPP;
     }
   }
 
+
+  //print all words in dictionary
+  /*
+  for(int i = 0; i < MAXLINES; i++){
+    printf("%s \n", dictionary[i]);
+  }
+  */
+  printf("YOU SPELLED WRONGLY :\n");
+  printLL(head);
+
   return 0;
 }
 
-bool isSpelledCorrectly(char word[]){
+
+// returns 0 for false, returns 1 for true
+int isSpelledCorrectly(char word[]){
   // get init range
   int min, mid, max;
   for(int i = 0; charLine[i].letter != word[0]; ++i){
@@ -86,10 +101,15 @@ bool isSpelledCorrectly(char word[]){
     max = charLine[i+2].fileline;
   }
   mid = (min + max ) / 2; // was going to add different searching algorithm
+
   // linear search through
-  for(min; min <= max; ++min){
-    // make an array storing all words and then search through it
-  }
+  for(min; min <= max ; ++min){
+    printf("COMPARING \"%s\" to -> \"%s\"  %s  \n", word, dictionary[min] , word[0] == dictionary[min][0] ? "true" : "false");
+    int i = 0;
+    if ( word == dictionary[min] )
+      return 1;
+    }
+  return 0;
 }
 
 
@@ -106,16 +126,20 @@ void loading(){
 
 
 void loadDictionary(){
-  // read file
+  // read file and load to array
 
   FILE *fp;
 
   if ( (fp = fopen("english.txt", "r")) == NULL){
     printf("cant open %s\n", "english.txt");
-    return 1;
+    //return 1;
   }else{
     int c, nl, state, arraycounter;
     int oldc = 'a';
+
+    int i = 0;
+    int globali = 0;
+    char string[45];
 
     state = OUTPP;
     nl = arraycounter = 0;
@@ -124,8 +148,12 @@ void loadDictionary(){
     charLine[arraycounter].letter = oldc;
     charLine[arraycounter++].fileline = nl;
 
+
+
     while((c = getc(fp)) != EOF){
       if(c == '\n'){
+        string[i++] = '\0';
+        strcpy(dictionary[globali++], string);
         ++nl;
         state = OUTPP; // this means new word is coming up next line
       }else if (state == OUTPP){
@@ -135,12 +163,39 @@ void loadDictionary(){
           charLine[arraycounter].letter = c;
           charLine[arraycounter++].fileline = nl;
         }
+        i = 0;
+        string[i++] = c;
+      }else{
+        string[i++] = c;
       }
     }
     // print array of structs
+
     for(int i = 0; i < 26; i++){
       printf("%c is at line %d\n", charLine[i].letter, charLine[i].fileline);
     }
+
   }
   fclose(fp);
+}
+
+
+void insertNode(struct Node* head, char val[]){
+  struct Node *current = head;
+  while(current->next != NULL)
+    current = current->next;
+
+  // add new varaible
+  current->next = (struct Node *) malloc(sizeof(struct Node));
+  strcpy(head->data, val);
+  current->next->next = NULL;
+}
+
+
+
+void printLL(struct Node* n){
+    while (n != NULL) {
+        printf(" %s \n", n->data);
+        n = n->next;
+    }
 }
