52f512a0df85cd65e3929e66ee1701f03e21f17a
Master47474 <meoro@hotmail.com.au>
2020-02-09 10:26:42 +1100

    magic 8 ball done

--- C/MiniProjects/magic8ball/main.c
+++ C/MiniProjects/magic8ball/main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <windows.h>
+#include <time.h>
+#include <stdlib.h>
 
 
 void loading(int buffer);
+int randInteger();
 
-char responses[][] = {"Certin", "Plausable", "unsure", "probably not", "Impossible"};
+
+
+char* responses[] = {"Certin", "Plausable", "unsure", "probably not", "Impossible"};
 
 int main(int argc, char *argv[]){
-  
+  printf("What question do you seek information about ? >> ");
+  char question[50];
+
+  while(1){
+    int c, i = 0;
+    while((c=getchar()) != EOF){
+      if( c == '\n')
+        break;
+      question[i++] = c;
+    }
+    question[i] = '\0';
+    if(strcmp("quit", question) == 0)
+      break;
+    loading(12);
+    printf("\n>> %s \n", responses[randInteger()]);
+    printf("What question do you seek information about ? >> ");
+  }
+
 }
 
 
@@ -19,10 +41,16 @@ void loading(int buffer){
     putchar('.');
     if(dotlength == 6){
       printf("\r       \r");
-      if (x == buffer)
-        break;
       dotlength = 0;
     }
+    if (x == buffer)
+      break;
     Sleep(400);
   }
+}
+
+
+int randInteger(){
+  srand(time(0));
+  return (rand() % 5);
 }
