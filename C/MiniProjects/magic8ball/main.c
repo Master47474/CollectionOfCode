2a641051617840d80c375f6639255fad97f96bc8
Master47474 <meoro@hotmail.com.au>
2020-02-08 13:53:43 +1100

    magic 9 ball
    starting magic 8 ball

---
+++ C/MiniProjects/magic8ball/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <windows.h>
+
+
+void loading(int buffer);
+
+char responses[][] = {"Certin", "Plausable", "unsure", "probably not", "Impossible"};
+
+int main(int argc, char *argv[]){
+
+}
+
+
+
+void loading(int buffer){
+  int x = 0;
+  for (int dotlength = 0; dotlength < 7; dotlength++){
+    x++;
+    putchar('.');
+    if(dotlength == 6){
+      printf("\r       \r");
+      if (x == buffer)
+        break;
+      dotlength = 0;
+    }
+    Sleep(400);
+  }
+}
