ec4336c487b0494eec51e1ebcd1855841deafa07
Master47474 <meoro@hotmail.com.au>
2020-02-14 10:04:06 +1100

    comments

--- C/Learning/GUIprogamming/addingRecources/resource.h
+++ C/Learning/GUIprogamming/addingRecources/resource.h
@@ -1,3 +1,9 @@
 #define IDI_MYICON  101
 
 #define ID_FILE_EXIT	4001
+
+
+// to link rc
+// create windres rcfile.rc -o rcfile.o
+// make main an o file, gcc -c main.c -o main.o
+// create exe gcc -o Executable main.o res.o
