c457d89e8124658dd0f097d89669114a9c1e2226
Master47474 <meoro@hotmail.com.au>
2020-02-15 23:51:13 +1100

    dialog boxes
    with rescources

---
+++ C/Learning/GUIprogamming/DialogsBox/resource.h
@@ -0,0 +1,12 @@
+#define IDD_ABOUT 100
+#define IDR_MYMENU 101
+#define IDI_MYICON 201
+
+
+
+#define ID_FILE_EXIT 9001
+#define ID_STUFF_GO 9002
+#define ID_HELP_ABOUT 9003
+
+
+#define IDC_STATIC -1
