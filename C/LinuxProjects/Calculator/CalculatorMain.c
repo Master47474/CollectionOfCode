b23e4fc6fccd46269414ed7b7ed544ef7c1b9b73
Master47474 <meoro@hotmail.com.au>
2020-02-28 00:31:31 +1100

    Revert "revert"
    This reverts commit dcdf6b1dee1aa97a167a212179b49dc017d87722.

--- C/LinuxProjects/Calculator/CalculatorMain.c
+++ C/LinuxProjects/Calculator/CalculatorMain.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 
 #define INPUTBUFSIZE 1048
+#define TRUE 1
+#define FALSE 0
+
 
 #define DIGITS "0123456789"
 
@@ -9,42 +12,59 @@
 char* captureInput(void);
 void printDebug(char* string);
 
+// operation functions and enums
 typedef enum OperationsPrec{
+	OPERERROR = -1,
 	EXPONENTIAL = 0,
        	MULTIPLICATION = 1,
        	DIVISION = 1,
        	ADDITION = 2,
-	SUBTRACTION = 2
-}
-
+	SUBTRACTION = 2 
+} OperationsPrec;
 
 const static struct{
 	OperationsPrec oper;
-	char symbol;
+	int symbol;
 } conversion[] = {
-}
+	{EXPONENTIAL, '^'},
+	{MULTIPLICATION, '*'},
+	{DIVISION, '/'},
+	{ADDITION   , '+'},
+	{SUBTRACTION, '-'},
+};
+
+OperationsPrec operation2Enum(const int symbol);
+int isOperation(const int symbol);
+
+//misc function declarations
+int isLetter(const int letter);
 
 
 
+
+
+// main
 
 int main(void){
 
 	char* input;
-
-	input = captureInput();
-
+	
+	input = captureInput();	
+	
 	printDebug(input);
 
-
+	
 	return EXIT_SUCCESS;
 }
 
 // redo capture input
+// only works with Ints no floats as of yet
 char* captureInput(void){
 	int bufsize = INPUTBUFSIZE;
 	int pos = 0;
 	char* input = malloc(sizeof(char) * bufsize);
 	int c;
+	char* tempFloatString;
 
 	if(!input){
 		printf("Allocation Error\n");
@@ -53,35 +73,23 @@ char* captureInput(void){
 
 	while(1){
 		c = getchar();
-
+		
 		if(c == ' ') continue;
+		if(isLetter(c)) continue;
 		if(c == EOF || c == '\n'){
 			input[pos++] = '\0';
 			return input;
 		}else{
-			if(anOperation(input[pos]) || aParenthisis(input[pos])){
-				// do something
-			}else if(isDigit(input[pos]) || c == '.'){
+			if(isOperation(c) ){//|| aParenthisis(input[pos])){
+				input[pos] = c;
+				printf("%c ,", c);// do something 	
+			}//else if(isDigit(input[pos]) || c == '.'){
 				//append to placeholder string for floats
-			}
-
-		}
-		pos++;
-
-			if(anOperation(input[pos++]))
-			       //do something that would be like adding it to input
-			//else if it is a digit add it do a string
-			//
-			input[pos++] = c;
+			//}
+			
 		}
 
-			if(anOperation(input[pos++]))
-			       //do something that would be like adding it to input
-			//else if it is a digit add it do a string
-			//
-			input[pos++] = c;
-		}
-
+		
 		if(pos >= bufsize){
 			bufsize += INPUTBUFSIZE;
 			input = realloc(input, bufsize);
@@ -90,6 +98,7 @@ char* captureInput(void){
 				exit(EXIT_FAILURE);
 			}
 		}
+		pos++;
 	}
 	return input;
 }
@@ -102,3 +111,34 @@ void printDebug(char* string){
 		printf("\"%c\", ",string[i++]);
 	printf("}\n");
 }
+
+
+
+OperationsPrec operation2Enum(const int symbol){
+	int j;
+	for(j = 0; j< sizeof(conversion)/sizeof(conversion[0]); ++j)
+		if( symbol == conversion[j].symbol)
+			return conversion[j].oper;
+	return OPERERROR;
+}
+
+
+int isOperation(const int symbol){
+	if(operation2Enum(symbol) >= 0)
+		return TRUE;
+	return FALSE;
+}
+
+int isLetter(const int letter){
+	if((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
+		return TRUE;
+	return FALSE;
+}
+
+
+
+
+
+
+
+
