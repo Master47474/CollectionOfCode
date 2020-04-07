f17ca2b891e674f0f019103a357414795062e8cf
Master47474 <meoro@hotmail.com.au>
2020-04-07 23:11:36 +1000

    Fixed Memory issue with list of pointer tokens

--- C/LinuxProjects/Calculator/TestFunctions/testing.c
+++ C/LinuxProjects/Calculator/TestFunctions/testing.c
@@ -20,25 +20,26 @@ int main(){
 		free(oper);
 	}
 
-	opertok** operationtokList = malloc(sizeof(opertok*) * 1024);
+	opertok** operationtokList = malloc(sizeof(opertok*) * 50);//= (opertok**)malloc(sizeof(opertok*) * 1024);
 	int listi = 0;
 	int j = 0;
 	while(iterate[j] !=  '\0'){
-		char* oper = malloc(sizeof(char) * 2);
+		char* oper = (char*)malloc(sizeof(char) * 2);
 		oper[0] = iterate[j++];
 		oper[1] = '\0';
-		opertok operationtok = createOperationToken(&oper);
-		operationtokList[listi] = malloc(sizeof(operationtok));
-		operationtokList[listi++] = &operationtok;
-		printf("what is this %s\n", operationtokList[listi-1]->operid);
+		operationtokList[listi] = malloc(sizeof(opertok));
+		printf("SUP \n");
+		operationtokList[listi++]->operid = oper;
 	}
-
+	operationtokList[listi] = malloc(sizeof(opertok));
+	operationtokList[listi]->operid = "\0";
 	printf("what about here\n");
 	int loop = 0;
 	printf("TRY THIS %s\n", operationtokList[0]->operid);
 	while(strcmp(operationtokList[loop]->operid, "\0")){
-		printf("loop working      ;");
+		printf("loop working      ;\n");
 		printf("%s\n", operationtokList[loop++]->operid);
 	} 
+	printf("WE MADE IT\n");
 	return 0;
 }
