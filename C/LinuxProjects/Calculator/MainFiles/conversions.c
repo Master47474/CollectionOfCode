20f65d91797a7211b72e9cbc0a4cc45e32860a5d
Master47474 <meoro@hotmail.com.au>
2020-06-23 22:14:02 +1000

    Fixed Bug with conversions , Addition of 2 difference alpha not working

--- C/LinuxProjects/Calculator/MainFiles/conversions.c
+++ C/LinuxProjects/Calculator/MainFiles/conversions.c
@@ -7,9 +7,15 @@
 
 int strToint(char* string){
 	//for each char turn into int
+	int digits = 0;
+	for(digits = 0; string[digits] != '\0'; digits++)
+		;
+	digits--;
+
 	int converted = 0;
 	for(int i = 0; string[i] != '\0'; i++){
-		converted += (string[i]-48) * (int)pow(10,i);
+		converted += (string[i]-48) * (int)pow(10,digits--);
+		printf("-> %d", converted);
 	}
 	return converted;
 }
@@ -24,11 +30,12 @@ char* intTostr(int invalue, int base){
 
 	char* converted = (char*)malloc(sizeof(char) * (digits+1));
 	int difference = 0;
-	for(int i = 0; i < digits; i++){
-		int power = (int)pow(base,(digits-1)-i);
-		converted[i] = ((invalue-difference)/power) -48; //to make sure its the int
-		difference += (converted[i]+48) * base;
+	digits--;
+	for(int i = 0; i <= digits; i++){
+		int power = (int)pow(base,(digits-i));
+		converted[i] = (char)((invalue-difference)/power) + 48; //to make sure its the int
+		difference += (int)(converted[i]-48)*power;
 	}
-	converted[digits] = '\0';
+	converted[digits+1] = '\0';
 	return converted;
 }
