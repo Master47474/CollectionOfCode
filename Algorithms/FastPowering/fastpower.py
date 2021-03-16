a230750997aad079b329cd9880749f33a9f3a35b
Marcus <mrmarcusfacc@outlook.com>
2021-03-16 19:04:34 +1100

    Fast Power Algorithm
    An Algorithm for computing g^A (Mod m)

---
+++ Algorithms/FastPowering/fastpower.py
@@ -0,0 +1,31 @@
+
+g = 9
+A = 1892
+m = 10911
+"""
+print("Lets Fast Power (Modulo N)")
+g = int(input("input the base >"))
+A = int(input("input the exponent >"))
+m = int(input("input the modulo >"))
+"""
+
+# break the A (power) into its binary components
+Ab = bin(A)
+strAb = str(Ab)[2:]
+lstrAb = len(strAb)-1
+
+#compute g^(2^A_i) Mod N for all A_i and multiply then modulo
+solution = 1
+for bit in range(0, len(strAb)):
+    #get the binary value (2^i)
+    if(strAb[lstrAb - bit] == "1"):
+        binaryValue = 2**(bit)
+        # place g ^ (2^i) then MOD m
+        basePowBVmod = (g**binaryValue) % m
+        # multiply the current product value to g ^ (2^i)
+        solution *= basePowBVmod
+        # Solution MOD m to be consistent
+        solution = solution % m
+
+
+print(solution)
