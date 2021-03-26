6eb84db7b9f786b28084841cb03c441f8655c243
Marcus <mrmarcusfacc@outlook.com>
2021-03-26 11:46:57 +1100

    Update main.py
    aaaa

--- Python/OrderOfZ/main.py
+++ Python/OrderOfZ/main.py
@@ -2,7 +2,7 @@
 
 
 
-def ordofZ(sizeG):
+def ordofZ(sizeG, ordOf = None):
     result = [1] * sizeG
     for z in range(1, sizeG):
         i = 1
@@ -12,10 +12,15 @@ def ordofZ(sizeG):
             temp += z
         result[z] = i
 
+    NordOf = 0
     for i in range(sizeG):
+        if(result[i] == ordOf):
+            NordOf += 1
         print(i , ":" , result[i])
 
+    if(ordOf != None):
+        print(f"Number of order {ordOf} is {NordOf}")
 
 
 
-ordofZ(36)
+ordofZ(168, 84)
